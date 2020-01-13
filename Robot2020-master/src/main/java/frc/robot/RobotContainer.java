6d21d64794b4ddf7f972267ae710ff13b9136558
maxeitelwein <59778550+maxeitelwein@users.noreply.github.com>
2020-01-12 22:57:36 -0800

    TankDrive command finished
    TankDrive command should now work

--- Robot2020-master/src/main/java/frc/robot/RobotContainer.java
+++ Robot2020-master/src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,10 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.DriveBase;
-import edu.wpi.first.wpilibj2.command.Command;
-
 
 public class RobotContainer {
   private static XboxController m_controller = new XboxController(Constants.Controller.CONTROLLER_PORT);
@@ -23,17 +19,19 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureButtonBindings();
-    m_drivebase = new DriveBase(new TalonSRX(Constants.Motors.MOTOR_LEFT_1_ID),
-                                new TalonSRX(Constants.Motors.MOTOR_LEFT_2_ID),
-                                new TalonSRX(Constants.Motors.MOTOR_RIGHT_1_ID),
-                                new TalonSRX(Constants.Motors.MOTOR_RIGHT_2_ID));
+    m_drivebase = new DriveBase(new PWMTalonSRX(Constants.Motors.MOTOR_LEFT_1_ID),
+                                new PWMTalonSRX(Constants.Motors.MOTOR_LEFT_2_ID),
+                                new PWMTalonSRX(Constants.Motors.MOTOR_RIGHT_1_ID),
+                                new PWMTalonSRX(Constants.Motors.MOTOR_RIGHT_2_ID));
 
-    m_tankDrive = new TankDrive(m_drivebase);
+    m_tankDrive = new TankDrive(m_drivebase, 
+                                Constants.Controller.LEFT_STICK_Y, 
+                                Constants.Controller.RIGHT_STICK_Y);
   }
   
   private void configureButtonBindings() {}
   
-  public static XboxController Controller(){
+  public static XboxController getController(){
     return m_controller;
   }
 }
--- Robot2020-master/src/main/java/frc/robot/commands/TankDrive.java
+++ Robot2020-master/src/main/java/frc/robot/commands/TankDrive.java
@@ -1,14 +1,18 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveBase;
 
 public class TankDrive extends CommandBase{
   private DriveBase m_driveBase;
+  private int m_LeftStickY, m_RightStickY;
 
-  public TankDrive(DriveBase driveBase){
+  public TankDrive(DriveBase driveBase, int LeftStickY, int RightStickY){
     m_driveBase = driveBase;
+    m_LeftStickY = LeftStickY;
+    m_RightStickY = RightStickY;
     addRequirements(m_driveBase);
   }
 
@@ -19,6 +23,11 @@ public class TankDrive extends CommandBase{
 
   @Override
   public void execute(){
-    m_driveBase.drive(
+    XboxController xboxController = RobotContainer.getController();
+    m_driveBase.drive(xboxController.getRawAxis(m_LeftStickY), xboxController.getRawAxis(m_RightStickY));
   }
+  @Override
+	public boolean isFinished() {
+		return false;
+	}
 }
