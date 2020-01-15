1e9cfd3b14142f8ac9f3048568f8d0687e55cbb1
maxeitelwein <59778550+maxeitelwein@users.noreply.github.com>
2020-01-15 10:00:03 -0800

    Changed TalonSRX to WPI_TalonSRX

--- Robot2020-master/src/main/java/frc/robot/subsystems/FlyWheel.java
+++ Robot2020-master/src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,8 +9,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FlyWheel extends SubsystemBase{
 
-    private TalonSRX flywheel_Motor1;
-    private TalonSRX flywheel_Motor2;
+    private WPI_TalonSRX flywheelMotor1;
+    private WPI_TalonSRX flywheelMotor2;
     public final PIDController flyWheelPid;
 
     private final double kP = 0;
@@ -21,15 +20,14 @@ public class FlyWheel extends SubsystemBase{
 
     public FlyWheel(final WPI_TalonSRX motor1, final WPI_TalonSRX motor2) {
 
-        flywheel_Motor1 = motor1;
-        flywheel_Motor2 = motor2;
+        flywheelMotor1 = motor1;
+        flywheelMotor2 = motor2;
 
-        error = flywheel_Motor1.getSelectedSensorVelocity();
+        flywheelMotor2.follow(flywheelMotor1);
+        error = flywheelMotor1.getSelectedSensorVelocity();
 
         flyWheelPid = new PIDController(kP, kI, kD);
     }
-
-
 
     public void setRPM(double targetRPM){
         flyWheelPid.setSetpoint(targetRPM);
@@ -41,8 +39,7 @@ public class FlyWheel extends SubsystemBase{
 
     public void set_FlyWheel(double value)
     {
-        flywheel_Motor1.set(ControlMode.PercentOutput, value);
-        flywheel_Motor2.set(ControlMode.PercentOutput, value);
+        flywheelMotor1.set(ControlMode.PercentOutput, value);
     }
 
     public void periodic() {
