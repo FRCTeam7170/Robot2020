0a1897e84d567b17370ab145b92e704268a8a903
maxeitelwein <59778550+maxeitelwein@users.noreply.github.com>
2020-01-15 13:15:30 -0800

    Started to config Talon PID for flywheel

--- Robot2020-master/src/main/java/frc/robot/subsystems/FlyWheel.java
+++ Robot2020-master/src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.SensorTerm;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -24,6 +28,23 @@ public class FlyWheel extends SubsystemBase{
         flywheelMotor2 = motor2;
 
         flywheelMotor2.follow(flywheelMotor1);
+
+        flywheelMotor1.setNeutralMode(NeutralMode.Brake);
+        flywheelMotor2.setNeutralMode(NeutralMode.Brake);
+
+        flywheelMotor2.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+        flywheelMotor1.configRemoteFeedbackFilter(flywheelMotor2.getDeviceID(),
+                                                  RemoteSensorSource.TalonSRX_SelectedSensor, 0, 30);
+
+        flywheelMotor1.configSensorTerm(SensorTerm.Sum0, FeedbackDevice.RemoteSensor0, 30);
+        flywheelMotor1.configSensorTerm(SensorTerm.Sum1, FeedbackDevice.QuadEncoder, 30);
+
+        flywheelMotor1.configSelectedFeedbackSensor(FeedbackDevice.SensorSum, 0, 30);
+
+        flywheelMotor1.configSelectedFeedbackCoefficient(1, 0, 30);
+
+        
+
         error = flywheelMotor1.getSelectedSensorVelocity();
 
         flyWheelPid = new PIDController(kP, kI, kD);
