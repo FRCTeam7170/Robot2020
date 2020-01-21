30cb39644aa062e24e3470f799afbee02aa798c5
maxeitelwein <59778550+maxeitelwein@users.noreply.github.com>
2020-01-20 20:06:29 -0800

    venodrdpes removed from .gitignore

--- Robot2020-master/src/main/java/frc/robot/Robot.java
+++ Robot2020-master/src/main/java/frc/robot/Robot.java
@@ -23,8 +23,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
